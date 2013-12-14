23e451016664785e638e71f4e2dc894c3c88a204
biril <alexlambiris@gmail.com>
2013-12-14 21:37:03 +0100

    Bump patch number -> 0.10.1

--- backbone-faux-server.js
+++ backbone-faux-server.js
@@ -1,4 +1,4 @@
-//     Backbone Faux Server v0.10.0
+//     Backbone Faux Server v0.10.1
 
 //     https://github.com/biril/backbone-faux-server
 //     Licensed and freely distributed under the MIT License
@@ -562,7 +562,7 @@
          * @return {string} Current version of the library
          */
         getVersion: function () {
-            return "0.10.0"; // Keep in sync with package.json
+            return "0.10.1"; // Keep in sync with package.json
         }
     });
 
