cfb2eacedb6db74f0a55dca8c413656646599fe4
biril <alexlambiris@gmail.com>
2014-09-22 00:41:38 +0200

    Change testrunner logging options

--- test/testrunner.js
+++ test/testrunner.js
@@ -16,6 +16,32 @@ qunit.options.deps = [{
     namespace: "Backbone"
 }];
 
+qunit.options.log = {
+    // log assertions overview
+    assertions: false,
+
+    // log expected and actual values for failed tests
+    errors: true,
+
+    // log tests overview
+    tests: true,
+
+    // log summary
+    summary: true,
+
+    // log global summary (all files)
+    globalSummary: true,
+
+    // log coverage
+    coverage: true,
+
+    // log global coverage (all files)
+    globalCoverage: true,
+
+    // log currently testing code file
+    testing: true
+};
+
 qunit.run({
     code: { path: absPath("../backbone-faux-server.js"), namespace: "fauxServer" },
     tests: [
